f767135195d092103d127084541f5ac20cd60fe9
Leonardo Rocha <leonardocar.dm@gmail.com>
2020-09-08 13:49:30 -0400

    Add package axios Add api connection in Dashboard page Change Repository page and add api connection

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,72 +1,95 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
 
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const repositoriesStorageKey = '@GithubExplorer:repositories';
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(repositoriesStorageKey);
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(repositoriesStorageKey, JSON.stringify(repositories));
+  }, [repositories]);
+
+  async function handleAddRepository(
+    event: React.FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`/repos/${newRepo}`);
+
+      setRepositories([...repositories, response.data]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca por esse repositório');
+    }
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no github.</Title>
 
-      <Form action="">
-        <input placeholder="Digite o nome do repositório" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/38355333?s=460&u=ad01b38c2912eb463b043252261d49787ccaef8b&v=4"
-            alt="Leonardo Rocha"
-          />
+        {repositories.map(repository => (
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
 
-          <div>
-            <strong>elf-reader</strong>
-            <p>
-              Elf-reader is a tool for reading executable (ELF) files and
-              creating a bootable OS image.
-            </p>
-          </div>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/38355333?s=460&u=ad01b38c2912eb463b043252261d49787ccaef8b&v=4"
-            alt="Leonardo Rocha"
-          />
-
-          <div>
-            <strong>elf-reader</strong>
-            <p>
-              Elf-reader is a tool for reading executable (ELF) files and
-              creating a bootable OS image.
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/38355333?s=460&u=ad01b38c2912eb463b043252261d49787ccaef8b&v=4"
-            alt="Leonardo Rocha"
-          />
-
-          <div>
-            <strong>elf-reader</strong>
-            <p>
-              Elf-reader is a tool for reading executable (ELF) files and
-              creating a bootable OS image.
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
       </Repositories>
     </>
   );
